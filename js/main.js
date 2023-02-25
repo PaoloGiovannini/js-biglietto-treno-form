b18e70443ff8bbe8ea89e2db10acb4974ee8ac8b
Paolo Giovannini <giovannini13@gmail.com>
2023-02-25 14:14:52 +0100

    esercizio concluso

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const generaBigliettoDom = document.getElementById('generaBiglietto');
 
+const resettaBigliettoDom = document.getElementById ('resettaBiglietto')
+
 const etaDom = document.getElementById('eta');
 
 const kmDom = document.getElementById('km');
@@ -32,18 +34,39 @@ generaBigliettoDom.addEventListener('click',
 
         if (eta == "minorenne"){
             prezzoTotale = prezzoTotale - (prezzoTotale /100 * 20);
+            tipoBigliettoDom.innerHTML = "Sconto 20% per minorenni";
+
         } else if (eta == "senior") {
             prezzoTotale = prezzoTotale - (prezzoTotale /100 * 40);
-        } 
+            tipoBigliettoDom.innerHTML = "Sconto 40% per over65";
+        } else{
+            tipoBigliettoDom.innerHTML = "Biglietto standard";
+        }
 
         bigliettoDom.classList.remove('d-none');
         
         nomeDom.innerHTML = valoreNome;
 
-        costoBigliettoDom.innerHTML = `${prezzoTotale.toFixed(2)} €`
+        costoBigliettoDom.innerHTML = `${prezzoTotale.toFixed(2)} €`;
+
+        const carrozza = Math.floor(Math.random() * 10) + 1;
+        
+        CarrozzaDom.innerHTML = carrozza;
+
+        const codice = Math.floor(Math.random() * (100000 - 90000)) + 90000;
+        codicePrenotazioneDom.innerHTML = codice;
    
     }
 
 
+)
+
+resettaBigliettoDom.addEventListener('click',
+    function(){
+        bigliettoDom.classList.add('d-none');
+        nomeCognomeDom.value = "";
+        kmDom.value = "";
+        etaDom.value = "maggiorenne";
+    }
 )
 
